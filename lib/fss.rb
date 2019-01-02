4b2cad80cf386a3cce896e684164edc94f92575b
Jovanny Cruz <jovannypcg@yahoo.com>
2019-01-02 12:10:45 -0600

    Add bucket_exists? method

--- lib/fss.rb
+++ lib/fss.rb
@@ -6,4 +6,14 @@ module FSS
   def list_buckets
     minio.list_buckets
   end
+
+  def bucket_exists?(opts = {})
+    begin
+      minio.head_bucket(bucket: opts[:bucket])
+    rescue Aws::S3::Errors::NotFound
+      return false
+    end
+
+    true
+  end
 end
